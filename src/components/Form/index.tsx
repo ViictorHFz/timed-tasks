d52169817304492c03ea62d4d1a3f9d5d16be06c
Victor <victorsantos.hf@gmail.com>
2022-06-28 12:02:42 -0300

    refatoração CSS Modules

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Button from "../Button";
-import './style.scss';
+import style from './Form.module.scss';
 
 class Formulario extends React.Component {
   render() {
     return (
-      <form className="novaTarefa">
-        <div className="inputContainer">
+      <form className={style.novaTarefa}>
+        <div className={style.inputContainer}>
           <label htmlFor="tarefa">Adicione uma tarefa</label>
           <input
             type="text"
@@ -17,7 +17,7 @@ class Formulario extends React.Component {
           />
         </div>
 
-        <div className="inputContainer">
+        <div className={style.inputContainer}>
           <label htmlFor="tempo">Tempo</label>
           <input type="time" step="1" name="tempo" id="tempo" min="00:00" max="1:30:00" required />
         </div>
