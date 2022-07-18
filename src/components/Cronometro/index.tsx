4a35a6c0cf4e675dca210473235242d3989e2230
Victor <victorsantos.hf@gmail.com>
2022-07-18 17:19:37 -0300

    refatorando

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "../Button";
 import Relogio from "./Relogio";
 import style from "./Cronometro.module.scss"
-import { tempoParaSegundos } from "../../common/utils/date";
+import { tempoParaSegundos } from "../../common/utils/time";
+import { ITarefa } from "../../types/ITarefa";
 
-export default function Cronometro() {
+interface Props {
+  selecionado: ITarefa | undefined
+}
 
-  console.log('conversao :', tempoParaSegundos('01:01:01'))
+export default function Cronometro({ selecionado }: Props) {
+  const [tempo, setTempo] = useState<number>();
+
+  useEffect(() => {
+    if(selecionado?.tempo) {
+      setTempo(tempoParaSegundos(selecionado.tempo))
+    }
+  }, [selecionado])
+
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha uma tarefa e inicie o cronômetro</p>
+      Tempo: {tempo}
       <div className={style.relogioWrapper}>
         <Relogio />
       </div>
